2a8e72a6129f75877fcd558d4f13cd9a9275c17d
Hiro Tanaka <HTanaka@lcs.on.ca>
2024-01-16 14:09:34 -0500

    Initial Commit

---
+++ Geometry Helper/Geometry_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Geometry_HelperApp.swift
+//  Geometry Helper
+//
+//  Created by 田中宏貴 on 2024-01-16.
+//
+
+import SwiftUI
+
+@main
+struct Geometry_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
